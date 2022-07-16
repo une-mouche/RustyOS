fd6360846949ba63768efe8704c2376fe2bda7c1
Clément LANATA <clement.lanata@epita.fr>
2022-07-16 18:49:36 +0200

    A tale of Dragons, Heroes and Legends...

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,13 @@ fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop{}
 }
